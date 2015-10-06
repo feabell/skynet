3d279f613e1bd6f0a5ef6bfc676dcd9bd9bef6f1
feabell <feabell@prod.home>
2015-10-06 20:54:20 +1300

    IGB test

--- skynet.py
+++ skynet.py
@@ -4,11 +4,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-	print request.headers	
+@app.route('/fleet/<fleetid>')
+def main(fleetid):
 	name = "Dakodai"
-	return render_template('skynet.html', name=name)
+	return render_template('skynet.html', name=name, fleetid=fleetid)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
